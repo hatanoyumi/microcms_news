f0c16f8fbb79d2b9f5605f4b9233bc1b6cd84941
Hatano Yumi <>
2025-04-24 16:47:47 +0900

    no message

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,7 @@ type Props = {
   id: string;
   title: string;
   publishedAt: string;
+  category: { name: string };
 };
 
 // microCMSからお知らせ記事を取得
@@ -16,7 +17,7 @@ async function getNewsPosts(): Promise<Props[]> {
   const data = await client.get({
     endpoint: 'news', // 'news'はmicroCMSのエンドポイント名
     queries: {
-      fields: 'id,title,publishedAt',  // idとtitleを取得
+      fields: 'id,category,title,publishedAt',  // idとtitleを取得
       limit: 15,  // 最新の5件を取得
     },
   });
@@ -29,13 +30,15 @@ export default async function Home() {
   return (
     <main>
       <h1>お知らせ記事一覧</h1>
-      <ul>
+      <ul className={styles.list}>
         {posts.map((post) => (
-          <li key={post.id} className={styles.list}>
+          <li key={post.id}>
             <Link href={`/news/${post.id}`}> {/* 記事へのリンクを生成 */}
+              {/* カテゴリを表示 */}
+              <div className={styles.category}>{post.category && post.category.name}</div>
               {post.title} {/* タイトルを表示 */}
               {/* 日付を表示 */}
-              <span className='style.date'>{dayjs(post.publishedAt).format('YY.MM.DD')}</span>
+              <span className={styles.date}>{dayjs(post.publishedAt).format('YY.MM.DD')}</span>
             </Link>
           </li>
         ))}
